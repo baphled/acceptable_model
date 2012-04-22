42c259e65994d9695d360401eb77bcf42815ac93
Yomi Colledge <baphled@boodah.net>
2012-04-22 01:53:42 +0100

    Extend the objects to_json to include its relationships

--- lib/acceptable_model.rb
+++ lib/acceptable_model.rb
@@ -1,11 +1,41 @@
 require "active_support/inflector"
-require "delegate"
 
-class AcceptableModel
+module AcceptableModel
   def self.define model
     dynamic_name = "Acceptable#{model.to_s.capitalize}"
     model_object = model.to_s.capitalize.constantize
     Object.const_set(dynamic_name, model_object)
+    dynamic_name.constantize.extend HateOS
+  end
+
+  module HateOS
+    module ClassMethods
+    end
+  
+    module InstanceMethods
+      attr_accessor :links
+
+      def links
+        [
+          {
+            :href => "/#{self.class.to_s.downcase.pluralize}/#{id}",
+            :rel => '/self'
+          }
+        ]
+      end
+
+      def to_json options = {}
+        opts = {
+          :links => links
+        }.merge! options
+        opts.to_json
+      end
+    end
+  
+    def self.included(receiver)
+      receiver.extend         ClassMethods
+      receiver.send :include, InstanceMethods
+    end
   end
 end
 
--- spec/acceptable_model_spec.rb
+++ spec/acceptable_model_spec.rb
@@ -1,10 +1,18 @@
 require "acceptable_model"
 
+require "json"
+
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :id
+  include AcceptableModel::HateOS
 
   def initialize params = {}
     self.name = params[:name]
+    self.id = self.name.downcase.gsub(' ', '-')
+  end
+
+  def to_json
+    super :name => name
   end
 end
 
@@ -26,6 +34,22 @@ describe AcceptableModel do
     it "exposes the originating models accessors" do
       model = AcceptableArtist.new :name => 'Busta Rhymes'
       model.name.should eql 'Busta Rhymes'
+    end
+  end
+
+  describe "#to_json" do
+    it "returns at HATEOS like format" do
+      expected = {
+        :links => [
+          {
+            :href => '/artists/busta-rhymes',
+            :rel => '/self'
+          }
+        ],
+        :name => 'Busta Rhymes'
+      }.to_json
+      model = AcceptableArtist.new :name => 'Busta Rhymes'
+      model.to_json.should eql expected
     end
   end
 end
