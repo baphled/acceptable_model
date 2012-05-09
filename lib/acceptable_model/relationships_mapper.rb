d6333355cfaa454a2200acf239fe3d3d3ee0f629
Yomi Colledge <baphled@boodah.net>
2012-05-09 17:49:00 +0100

    Complete the implementation of the representation mapper

--- lib/acceptable_model/relationships_mapper.rb
+++ lib/acceptable_model/relationships_mapper.rb
@@ -3,27 +3,55 @@ module AcceptableModel
     attr_accessor :model
     attr_accessor :associations
     attr_accessor :response_block
+    attr_accessor :attributes_block
 
     def initialize options = {}
       self.model = options[:model]
       self.associations = options[:associations]
+      self.attributes_block = options[:attributes_block]
       self.response_block = options[:response_block]
     end
 
+    #
+    # The representation of the model with its relationships and links.
+    #
+    # This method is used when we want to get a HATEOS like response.
+    #
+    def representation
+      representation = attributes
+      relationships.each { |relationship| representation.merge! relationship }
+      representation.merge :links => links
+    end
+
+    #
+    # Calls the attributes_block and passes the model to base on the response on
+    #
+    def attributes
+      attributes_block.call model
+    end
+
+    #
+    # Build the links structure which includes all the links relating to the model
+    #
+    # These include a link to the actual model along with its relationships
+    #
     def links
-      children = associations.collect { |association|
+      children = model.extended_relationships.collect { |association|
         model.send(association.to_sym).collect { |rel_model|
-          response_block.call rel_model, 'children'
+          response_block.call rel_model, association
         }
       }.flatten!
       children.unshift( response_block.call( model, 'self') )
     end
 
+    #
+    # Returns a list of all the models relationships
+    #
     def relationships
       associations.collect { |related_model|
         {
-          related_model => 
-          model.send(related_model).collect { |associated_model| 
+          related_model.to_sym => 
+          model.send(related_model.to_sym).collect { |associated_model| 
             associated_model.attributes.merge! :links => [ response_block.call(associated_model, 'children') ]
           }
         }
--- spec/acceptable_model/relationships_mapper_spec.rb
+++ spec/acceptable_model/relationships_mapper_spec.rb
@@ -1,9 +1,26 @@
 require "spec_helper"
 
 describe AcceptableModel::RelationshipsMapper do
-  let(:model) { AcceptableModel::Artist.new :name => 'Busta Rhymes', :aliases => ['Busta Bus'] }
+  let( :artist ) { 
+    params = {
+      :name => 'Busta Rhymes',
+      :groups => ['Flipmode Squad', 'Leaders of The New School']
+    }
+    AcceptableModel::Artist.new params
+  }
   let(:group1) { Group.new :name => 'Flipmode Squad', :id => 'flipmode-squad' }
   let(:group2) { Group.new :name => 'Leaders of The New School', :id => 'leaders-of-the-new-school' }
+  let(:structure) {
+    Proc.new do |model, relationship|
+      {
+        :href => "/#{model.class.to_s.downcase.pluralize}/#{model.id}",
+        :rel => "/#{relationship.camelize :lower}"
+      }
+    end
+  }
+  let(:associations) { AcceptableModel::Artist.associations }
+  let(:attributes) { artist.version_lookup( 'vnd.acme.artist-v1+json')[:attributes] }
+  let( :mapper ) { AcceptableModel::RelationshipsMapper.new :model => artist, :response_block => structure, :attributes_block => attributes, :associations => associations }
 
   before :each do
     AcceptableModel.define 'artist'
@@ -21,159 +38,138 @@ describe AcceptableModel::RelationshipsMapper do
         groups.all
       end
     end
-    model.groups.stub(:all).and_return [group1, group2]
+    artist.groups.stub(:all).and_return [group1, group2]
+  end
+
+  after :each do
+    class AcceptableModel::Artist
+      undef part_of
+    end
   end
 
   it "takes a model" do
     expect {
-      AcceptableModel::RelationshipsMapper.new :model => model
+      AcceptableModel::RelationshipsMapper.new :model => artist, :response_block => structure, :attributes_block => attributes, :associations => associations
     }.to_not raise_error Exception
   end
 
   it "exposes the model" do
-    mapper = AcceptableModel::RelationshipsMapper.new :model => model
-    mapper.model.should be_a AcceptableModel::Artist
+    mapper.model.should_not be_nil
+  end
+
+  it "knows what attributes should be returned" do
+    mapper.attributes.should_not include :groups=>["Flipmode Squad", "Leaders of The New School"]
   end
 
   describe "#response_block" do
-    let(:structure) {
-      Proc.new do |model, relationship|
-        {
-          :href => "/#{model.class.to_s.downcase.pluralize}/#{model.id}",
-          :rel => "/#{relationship.camelize :lower}"
-        }
-      end
-    }
-
     it "takes a structure block" do
       expected =
       {
         :href => '/artists/123',
         :rel => '/children'
       }
-      mapper = AcceptableModel::RelationshipsMapper.new :model => model, :response_block => structure
       mapper.response_block.call( stub(:class => Artist, :id => '123'), 'children' ).should eql expected
     end
   end
 
   describe "#associations" do
-    let( :artist ) { 
-      params = {
-        :name => 'Busta Rhymes',
-        :groups => ['Flipmode Squad', 'Leaders of The New School']
-      }
-      AcceptableModel::Artist.new params
-    }
-    let(:group1) { Group.new :name => 'Flipmode Squad', :id => 'flipmode-squad' }
-    let(:group2) { Group.new :name => 'Leaders of The New School', :id => 'leaders-of-the-new-school' }
-    let(:associations) { AcceptableModel::Artist.associations }
-
-    before :each do
-      class AcceptableModel::Artist
-        version ['vnd.acme.artist-v1+json', 'vnd.acme.artist-v1+xml'] do |artist|
-          {
-            :id => artist.id,
-            :name => artist.name
-          }
-        end
-
-        relationship :groups
-      end
-      artist.groups.stub(:all).and_return [group1, group2]
-    end
-
-    after :each do
-      class AcceptableModel::Artist
-        undef part_of
-      end
-    end
-
     it "can take the a list of model associations" do
-      mapper = AcceptableModel::RelationshipsMapper.new :model => artist, :associations => associations
       mapper.associations.should be_an Array
     end
   end
 
-  describe "#relationships" do
-    let( :artist ) { 
-      params = {
-        :name => 'Busta Rhymes',
-        :groups => ['Flipmode Squad', 'Leaders of The New School']
-      }
-      AcceptableModel::Artist.new params
-    }
-    let(:structure) {
-      Proc.new do |model, relationship|
-        {
-          :href => "/#{model.class.to_s.downcase.pluralize}/#{model.id}",
-          :rel => "/#{relationship.camelize :lower}"
-        }
-      end
-    }
-    let(:group1) { Group.new :name => 'Flipmode Squad', :id => 'flipmode-squad' }
-    let(:group2) { Group.new :name => 'Leaders of The New School', :id => 'leaders-of-the-new-school' }
-    let(:associations) { AcceptableModel::Artist.associations }
-
-    before :each do
-      class AcceptableModel::Artist
-        version ['vnd.acme.artist-v1+json', 'vnd.acme.artist-v1+xml'] do |artist|
-          {
-            "id " => artist.id,
-            "name" => artist.name
-          }
-        end
-
-        relationship :groups
-      end
-      artist.groups.stub(:all).and_return [group1, group2]
-    end
-
-    after :each do
-      class AcceptableModel::Artist
-        undef part_of
-      end
-    end
-
-    it "creates the hash for the model"
-
+  describe "#links" do
     it "creates the hash for the links" do
       links = [
         {:href=>"/artists/busta-rhymes", :rel=>"/self"},
-        {:href=>"/groups/flipmode-squad", :rel=>"/children"},
-        {:href=>"/groups/leaders-of-the-new-school", :rel=>"/children"}
+        {:href=>"/groups/flipmode-squad", :rel=>"/partOf"},
+        {:href=>"/groups/leaders-of-the-new-school", :rel=>"/partOf"}
       ]
-
-      mapper = AcceptableModel::RelationshipsMapper.new :model => artist, :associations => associations, :response_block => structure
       mapper.links.should eql links
     end
 
+  end
+
+  describe "#relationships" do
     it "creates the hash for the associations" do
-      expected = {
-        'groups' => [
-          {
-            :id => 'flipmode-squad',
-            :name => 'Flipmode Squad',
-            :links => [
-              {
-                :href => '/groups/flipmode-squad',
-                :rel => '/children'
-              }
-            ]
-          },
-          {
-            :id => 'leaders-of-the-new-school',
-            :name => 'Leaders of The New School',
-            :links => [
-              {
-                :href => '/groups/leaders-of-the-new-school',
-                :rel => '/children'
-              }
-            ]
-          }
+      expected = [
+        {
+          :groups => [
+            {
+              :id => 'flipmode-squad',
+              :name => 'Flipmode Squad',
+              :links => [
+                {
+                  :href => '/groups/flipmode-squad',
+                  :rel => '/children'
+                }
+              ]
+            },
+            {
+              :id => 'leaders-of-the-new-school',
+              :name => 'Leaders of The New School',
+              :links => [
+                {
+                  :href => '/groups/leaders-of-the-new-school',
+                  :rel => '/children'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+      mapper.relationships.should eql expected
+    end
+  end
+
+  describe "#representation" do
+    it "includes the models attributes" do
+      expected = {:id=>"busta-rhymes", :name=>"Busta Rhymes"}
+      mapper.representation.should include expected
+    end
+
+    it "includes the models associations" do
+      links = {
+        :links => [
+          {:href=>"/artists/busta-rhymes", :rel=>"/self"},
+          {:href=>"/groups/flipmode-squad", :rel=>"/partOf"},
+          {:href=>"/groups/leaders-of-the-new-school", :rel=>"/partOf"}
         ]
       }
-      mapper = AcceptableModel::RelationshipsMapper.new :model => artist, :associations => associations, :response_block => structure
-      mapper.relationships.should include expected
+      mapper.representation.should include links
+    end
+
+    it "includes the models relationships" do
+      expected = {
+          :groups => [
+            {
+              :id => 'flipmode-squad',
+              :name => 'Flipmode Squad',
+              :links => [
+                {
+                  :href => '/groups/flipmode-squad',
+                  :rel => '/children'
+                }
+              ]
+            },
+            {
+              :id => 'leaders-of-the-new-school',
+              :name => 'Leaders of The New School',
+              :links => [
+                {
+                  :href => '/groups/leaders-of-the-new-school',
+                  :rel => '/children'
+                }
+              ]
+            }
+          ]
+        }
+
+      mapper.representation.should include expected
+    end
+
+    it "should not include attributes we don't care about" do
+      mapper.attributes.should_not include :groups => ["Flipmode Squad", "Leaders of The New School"]
     end
   end
 end
